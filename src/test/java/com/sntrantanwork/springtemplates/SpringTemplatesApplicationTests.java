2281ff461efb995288c4ed25a301613acce49024
Tan Tran <sntran@Tans-MBP.attlocal.net>
2022-04-26 09:25:18 -0500

    Initial Commit

---
+++ src/test/java/com/sntrantanwork/springtemplates/SpringTemplatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sntrantanwork.springtemplates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTemplatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
